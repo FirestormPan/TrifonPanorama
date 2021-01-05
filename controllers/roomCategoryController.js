81f206b1ee0bd94b1582bbd7e64698df12bc48f6
Padelos <pantelos1999@gmail.com>
2021-01-05 19:19:37 +0200

    made the basic controller and the parts it communicates with

--- controllers/roomCategoryController.js
+++ controllers/roomCategoryController.js
@@ -1,13 +1,20 @@
 
-
-
-
-
-
-
-
-
 exports.room_info = function(req,res,next){
-   //if(req.params.categoryName !== null) 
-    res.render('roomCategory_Info', {categoryName: req.params.categoryName})
+   switch(req.params.categoryName){
+    case "Diklina":
+        //
+        break;
+    case "Monoklina":
+        //
+        break;
+    case "Spiti oloklhro":
+    //
+     break;
+    default:
+        res.status(404).send("category not found. try refreshing the page");
+        return;
+   }
+     
+    res.render('roomCategory_Info', {categoryName: req.params.categoryName , information:'here is my info', imagePath:'../images/flame-knight.jpg'} );
 };
+
